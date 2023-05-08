6ccd5d3f21c2abb7118137d4d1cb44910bc2ab33
Luís Fernando <luisfernandodepaulosousa0020@gmail.com>
2023-05-08 12:26:57 -0300

    Finalizado ex 03

--- ex03/main.js
+++ ex03/main.js
@@ -1,11 +1,14 @@
 function contar() {
+  document.querySelector('.display-contagem').textContent = ''
+  valorInicial = ''
+  valorFinal = ''
   var valorInicial = Number(document.querySelector('.comeco').value)
   var valorFinal = Number(document.querySelector('.final').value)
+  var passo = Number(document.querySelector('.passo').value)
   var displayResult = document.querySelector('.display-text')
-  for (i = valorInicial; i <= valorFinal; i++) {
+  for (i = valorInicial; i <= valorFinal; i += passo) {
     displayResult.textContent = "Contando:"
-    document.querySelector('.display-contagem').textContent += `${i} -  `
+    document.querySelector('.display-contagem').textContent += `${i}   `
   }
-
 }
 
